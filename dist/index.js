d77d9f37377f6b221a0a34eb5a9b4f0ff77f6ef0
jilink <jimmyfac1@gmail.com>
2021-10-27 10:05:06 +0200

    first publishing npm

---
+++ dist/index.js
@@ -0,0 +1,62 @@
+"use strict";
+
+require("core-js/modules/es.object.assign.js");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.SearchBar = void 0;
+
+require("core-js/modules/web.dom-collections.iterator.js");
+
+require("core-js/modules/es.string.includes.js");
+
+var _react = _interopRequireWildcard(require("react"));
+
+const _excluded = ["list", "setList", "filterField"];
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+const SearchBar = _ref => {
+  let {
+    list,
+    setList,
+    filterField = item => item
+  } = _ref,
+      props = _objectWithoutProperties(_ref, _excluded);
+
+  const [value, setValue] = (0, _react.useState)("");
+  (0, _react.useEffect)(() => {
+    if (value) {
+      setList(filterList());
+    } else {
+      setList(list);
+    }
+  }, [value]);
+
+  const filterList = () => {
+    return list.filter(item => filterField(item).toLowerCase().includes(value.toLocaleLowerCase()));
+  };
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
+  return /*#__PURE__*/_react.default.createElement("input", _extends({
+    type: "text",
+    placeholder: "search ...",
+    name: "search",
+    onChange: handleChange,
+    value: value
+  }, props));
+};
+
+exports.SearchBar = SearchBar;
